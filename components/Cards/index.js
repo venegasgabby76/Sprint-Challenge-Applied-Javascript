742e7e4e128139ebf89a3a4a296a3be359830960
Gabby Venegas <LJesus@Gabbys-Air-2.attlocal.net>
2020-04-17 18:58:23 -0700

    did some of the cards section but didnt finish it yet

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,41 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+
+
+  function cardCreator (item) {
+      const card = document.createElement('div')
+      card.classList.add('card')
+
+      const headline = document.createElement('div')
+      headline.classList.add('headline')
+      headline.textContent = items.headline
+
+      const author = document.createElement('div')
+      author.classList.add('author')
+
+      const imgContainer = document.createElement('div')
+      imgContainer.classList.add('img')
+
+      const image = document.createElement('img')
+      image.setAttribute( "src" , items.authorPhoto)
+
+      const name = document.createElement('div')
+      name.textContent = `By ${items.authorName}`
+
+
+
+      return card;
+  }
